9e75635e62b4caac4261b6e785145b1bb5967781
Andrey-Ryzhenkov <andreyavdukov@gmail.com>
2024-11-20 01:46:37 +0300

    init

--- src/main/java/com/example/springresttest/SpringRestTestApplication.java
+++ src/main/java/com/example/springresttest/SpringRestTestApplication.java
@@ -1,13 +1,35 @@
 package com.example.springresttest;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.springresttest.model.User;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
-public class SpringRestTestApplication {
+import static com.example.springresttest.service.UserServiceImpl.*;
+
+
+public class SpringRestTestApplication{
+
+    private static final String BASE_URL = "http://94.198.50.185:7081/api/users";
+    private static final RestTemplate restTemplate = new RestTemplate();
+    private static String sessionId;
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringRestTestApplication.class, args);
-    }
+        // Получить всех пользователей
+        getUsers(BASE_URL);
 
+        // Добавить пользователя
+        User newUser = new User(3L, "James", "Brown", (byte) 25);
+        String part1 = addUser(newUser,BASE_URL);
+
+        // Изменить пользователя
+        newUser.setName("Thomas");
+        newUser.setLastName("Shelby");
+        String part2 = updateUser(newUser,BASE_URL);
+
+        // Удалить пользователя
+        String part3 = deleteUser(3L,BASE_URL);
+
+        // Итоговый код
+        System.out.println("Result Code: " + part1 + part2 + part3);
+    }
 }
+
